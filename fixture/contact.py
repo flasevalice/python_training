4130161f6d76c9da7790113c2e281faae51dd250
flasevalice <55132480+flasevalice@users.noreply.github.com>
2023-06-16 20:09:06 +0300

    задание5 - Созданы классы-помощники и в них перенесены вспомогательные функции - Создан класс-менеджер, который инициализирует всех помощников

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,58 +1,12 @@
-from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
 
-class Application:
-    def __init__(self):
-        # self.wd = webdriver.Chrome(executable_path=r'')
-        self.wd = webdriver.Firefox(executable_path=r'D:\tools\geckodriver.exe')
-        self.wd.implicitly_wait(120)
-
-    def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self):
-        wd = self.wd
-        wd.get("http://localhost/addressbook/")
-
-    def create_group(self, group):
-        wd = self.wd
-        self.open_group()
-        wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
-        wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys(group.name)
-        wd.find_element_by_name("group_header").click()
-        wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys(group.logo)
-        wd.find_element_by_name("group_footer").click()
-        wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys(group.comment)
-        wd.find_element_by_name("submit").click()
-        self.return_to_groups_page()
-
-    def open_group(self):
-        wd = self.wd
-        wd.find_element_by_link_text("groups").click()
-
-    def logout(self):
-        wd = self.wd
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_groups_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("group page").click()
+class ContactHelper:
+    def __init__(self, app):
+        self.app = app
 
     def fill_full_contact_info(self, contact):
-        wd = self.wd
+        wd = self.app.wd
         self.to_add_contact_page()
         wd.find_element_by_link_text("add new").click()
         self.fill_contacts(contact)
@@ -64,21 +18,21 @@ class Application:
         self.to_home_page()
 
     def fill_contacts(self, contact):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_name("email").send_keys(contact.email)
         wd.find_element_by_name("email2").send_keys(contact.email2)
         wd.find_element_by_name("email3").send_keys(contact.email3)
         wd.find_element_by_name("homepage").send_keys(contact.homepage)
 
     def fill_phones(self, contact):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_name("home").send_keys(contact.home)
         wd.find_element_by_name("mobile").send_keys(contact.mobile)
         wd.find_element_by_name("work").send_keys(contact.work)
         wd.find_element_by_name("fax").send_keys(contact.fax)
 
     def fill_personal_info(self, contact):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("middlename").send_keys(contact.middlename)
         wd.find_element_by_name("lastname").send_keys(contact.lastname)
@@ -89,7 +43,7 @@ class Application:
         wd.find_element_by_name("address").send_keys(contact.address)
 
     def fill_dates(self, contact):
-        wd = self.wd
+        wd = self.app.wd
         Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
         Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
         wd.find_element_by_name("byear").send_keys(contact.byear)
@@ -99,19 +53,16 @@ class Application:
         wd.find_element_by_name("ayear").send_keys(contact.ayear)
 
     def fill_secondary_info(self, contact):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_name("address2").send_keys(contact.address2)
         wd.find_element_by_name("phone2").send_keys(contact.phone2)
         wd.find_element_by_name("notes").send_keys(contact.notes)
 
     def to_home_page(self):
-        wd = self.wd
+        wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
         wd.get("http://localhost/addressbook/index.php")
 
     def to_add_contact_page(self):
-        wd = self.wd
+        wd = self.app.wd
         wd.get("http://localhost/addressbook/edit.php")
-
-    def destroy(self):
-        self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from contact import Contact
-from application import Application
+from model.contact import Contact
+from fixture.application import Application
 import pytest
 
 
@@ -12,11 +12,11 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
-    app.fill_full_contact_info(Contact("n1", "mn1", "ln1", "nik1", "D:\Lol\Снимок3.png", "t1", "comp1", "addr1",
+    app.session.login(username="admin", password="secret")
+    app.contact.fill_full_contact_info(Contact("n1", "mn1", "ln1", "nik1", "D:\Lol\Снимок3.png", "t1", "comp1", "addr1",
                                        "1111111", "222222", "33333", "444444",
                                        "test@mail.ru", "test2@mail.ru", "test3@mail.ru", "http://test.ru",
                                        "1", "January", "1970", "2", "February", "1980",
                                        "addr2", "454545454", "note1"))
-    app.logout()
+    app.session.logout()
 
