b78a61a3bc4c60d6d4a5b7ecac4287fb2074e3c7
flasevalice <55132480+flasevalice@users.noreply.github.com>
2023-06-21 20:03:00 +0300

    задание8 - Реализована валидация фикстуры перед каждым тестом - Доработан вспомогательный класс, реализующий операции login и logout (SessionHelper) - Добавлена проверка текущего состояния сессии и выполнения действия с учётом текущего состояния.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,6 +2,4 @@ from model.group import Group
 
 
 def test_modify_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="New group"))
-    app.session.logout()
