0f7fc00da84072db420e3995144880a33bfb86ad
flasevalice <55132480+flasevalice@users.noreply.github.com>
2023-07-03 20:46:28 +0300

    задание15 - Параметризованы тесты для создания групп и контактов, чтобы в них использовались случайно сгенерированные строки для свойств создаваемых объектов.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,14 +1,41 @@
 # -*- coding: utf-8 -*-
+import pytest
+
 from model.contact import Contact
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_number(maxlen):
+    symbols = string.digits
+    return "+" + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_email(maxlen):
+    symbols = string.ascii_letters + string.digits
+    return "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])+"@mail.ru"
+
+
+test_data = [
+    Contact(firstname=random_string("fn", 10), middlename=random_string("mn", 10), lastname=random_string("ln", 10),
+            nickname=random_string("nik1", 10), photo="D:\Lol\Снимок3.png", title=random_string("t1", 10),
+            company=random_string("comp1", 10), address=random_string("addr1", 10),
+            home =random_number(10), mobile=random_number(10), work=random_number(10), fax =random_number(10),
+            email=random_email(10), email2=random_email(10), email3=random_email(10), homepage="http://test.ru",
+            bday="1", bmonth="January", byear="1970", aday="2", amonth="February", ayear="1980",
+            address2="addr2", phone2="454545454", notes="note1")
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("contact", test_data, ids=[repr(x) for x in test_data])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact("n1", "mn1", "ln1", "nik1", "D:\Lol\Снимок3.png", "t1", "comp1", "addr1",
-                                       "1111111", "222222", "33333", "444444",
-                                       "test@mail.ru", "test2@mail.ru", "test3@mail.ru", "http://test.ru",
-                                       "1", "January", "1970", "2", "February", "1980",
-                                       "addr2", "454545454", "note1")
     app.contact.fill_full_contact_info(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,23 +1,27 @@
 # -*- coding: utf-8 -*-
+import pytest
 from model.group import Group
+import random
+import string
 
 
-def test_add_group(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+test_data = [Group(name="", logo="", comment="")] + [
+    Group(name=random_string("name", 10), logo=random_string("logo", 10), comment=random_string("comment1", 10))
+    for i in range(5)
+]
+
+
+@pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    group = Group(name="one", logo="logo1", comment="comment1")
     app.group.create(group)
     assert len(old_groups)+1 == app.group.count()
     new_groups = app.group.get_group_list()
     old_groups.append(group)
 
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
-
-
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", logo="", comment="")
-    app.group.create(group)
-    assert len(old_groups) + 1 == app.group.count()
-    new_groups = app.group.get_group_list()
-    old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
