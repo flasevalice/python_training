f57487eb5db228583fc1d700d23ca87cd4b90972
flasevalice <55132480+flasevalice@users.noreply.github.com>
2023-07-11 20:53:51 +0300

    задание18 - зачатки задания по генерации данных

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,19 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
 from model.group import Group
-import random
-import string
-
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-
-test_data = [Group(name="", logo="", comment="")] + [
-    Group(name=random_string("name", 10), logo=random_string("logo", 10), comment=random_string("comment1", 10))
-    for i in range(5)
-]
+from data.add_group import test_data
 
 
 @pytest.mark.parametrize("group", test_data, ids=[repr(x) for x in test_data])
