ded1aebd72e885940e47e78661d2f977010f7842
flasevalice <55132480+flasevalice@users.noreply.github.com>
2023-07-14 21:42:21 +0300

    задание19 - выгрузка из json

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -5,8 +5,8 @@ from model.contact import Contact
 
 
 # @pytest.mark.parametrize("contact", test_data, ids=[repr(x) for x in test_data])
-def test_add_contact(app, data_contacts):
-    contact = data_contacts
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.fill_full_contact_info(contact)
     assert len(old_contacts) + 1 == app.contact.count()
