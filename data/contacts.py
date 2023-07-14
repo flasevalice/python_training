9162f8404b6f16d0458abb13bf43cfbf7519d3e2
flasevalice <55132480+flasevalice@users.noreply.github.com>
2023-07-14 21:31:15 +0300

    задание18 - сделан относительный путь для файла

--- data/contacts.py
+++ data/contacts.py
@@ -20,7 +20,7 @@ def random_email(maxlen):
 
 testdata = [
     Contact(firstname=random_string("fn", 10), middlename=random_string("mn", 10), lastname=random_string("ln", 10),
-            nickname=random_string("nik1", 10), photo="D:\Lol\Снимок3.png", title=random_string("t1", 10),
+            nickname=random_string("nik1", 10), photo="/Снимок3.png", title=random_string("t1", 10),
             company=random_string("comp1", 10), address=random_string("addr1", 10),
             home =random_number(10), mobile=random_number(10), work=random_number(10), fax =random_number(10),
             email=random_email(10), email2=random_email(10), email3=random_email(10), homepage="http://test.ru",
