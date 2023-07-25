48f767215e8987e9e0eb117eb57eedd441e6d18f
flasevalice <55132480+flasevalice@users.noreply.github.com>
2023-07-25 13:36:31 +0300

    Задание20 - добавлены проверки списков

--- fixture/db.py
+++ fixture/db.py
@@ -29,7 +29,7 @@ class DbFixture:
         cursor = self.connection.cursor()
         try:
             cursor.execute("select id, firstname, lastname, address, home, mobile, work, email, email2,"
-                           " email3, phone2 from addressbook")
+                           " email3, phone2 from addressbook where deprecated='0000-00-00 00:00:00'")
             for row in cursor:
                 (id, firstname, lastname, address, homephone, mobilephone, workphone, email, email2, email3,
                  phone2) = row
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -12,5 +12,6 @@ def test_add_contact(app, db, json_contacts, check_ui):
     # assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = db.get_contact_list()
     old_contacts.append(contact)
+    assert old_contacts == new_contacts
     if check_ui:
         assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
